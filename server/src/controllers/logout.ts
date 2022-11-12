962d2ee91b2281c607f1ac5a75ddda815ce72532
rouwe <santosroweme14@gmail.com>
2022-11-12 17:43:15 +0800

    Update backend logout to remove user specific created files

--- server/src/controllers/logout.ts
+++ server/src/controllers/logout.ts
@@ -1,15 +1,46 @@
 import { Request, Response } from 'express';
+import path from 'node:path';
+import fs from 'node:fs';
+import { usersCollection } from '../db/connect';
 import { cookieSessionId, cookieUserId } from '../utils/authUtil';
+import { UnknownUserError } from '../utils/errors';
 
-function logoutUserPostHandler(req: Request, res: Response) {
-    // Clear cookies
-    res.clearCookie(cookieSessionId);
-    res.clearCookie(cookieUserId);
-    res.json({
-        Response: {
-            status: "SUCCESS"
+async function logoutUserPostHandler(req: Request, res: Response) {
+    try {
+        // Validate user credential
+        const userId: string = req.cookies.u_Id;
+        const userData = await usersCollection.findOne({username: userId});
+        if (userData) {
+            // Delete user saved piczzles files
+            const userSavedDataArr: string[] = userData.saved;
+            userSavedDataArr.map((filename) => {
+                const filePath = `./upload/saved/${filename}`;
+                fs.unlink(filePath, (err) => {
+                    console.log("Deleting")
+                    if (err) throw new Error(`Failed to delete file: ${filename}`);
+                    console.log(`Deleted file: ${filePath}`);
+                });
+            })
+            // Clear cookies
+            res.clearCookie(cookieSessionId);
+            res.clearCookie(cookieUserId);
+            res.json({
+                Response: {
+                    status: "SUCCESS"
+                }
+            })
+        } else {
+            throw new UnknownUserError("User not found.");
         }
-    })
+
+    } catch(err) {
+        res.json({
+            Response: {
+                status: "FAILED",
+                error: err
+            }
+        })
+    }
 }
 
 export default logoutUserPostHandler;
